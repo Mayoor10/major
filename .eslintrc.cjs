adaacf75c227646a6fde01392eb3d15c27e6ce23
Mayoor10 <it.b.10.mayoor.moolya@gmail.com>
2024-01-13 22:00:22 +0530

    Authorization and Authentication BE

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 2022, // Specify the ECMAScript version
+    sourceType: 'module',
+  },
+  settings: { 
+    react: { version: '18.2' } 
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -17,4 +21,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
